de05c227d69f4c95d1e29b568ae0940fa908a6da
Peter Swallow <p.swallow@elsevier.com>
2019-02-28 19:54:44 +0000

    list of tags

--- HashCode.Photos/HashCode.Photos/Slide.cs
+++ HashCode.Photos/HashCode.Photos/Slide.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HashCode.Photos
 {
     public class Slide
     {
-        public List<Photo> Photos;
+        public List<Photo> Photos = new List<Photo>();
+
+        public IEnumerable<string> Tags
+        {
+            get { return Photos.SelectMany(photo => photo.Tags); }
+        }
+
+        public int TotalTags
+        {
+            get { return Tags.Count(); }
+        }
     }
 }
