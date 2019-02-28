df714869f7e05ed2fec51e01c17c6bf52fcef4a4
Peter Swallow <p.swallow@elsevier.com>
2019-02-28 19:35:23 +0000

    more tests

--- HashCode.Photos/HashCode.Tests/PhotoReaderTest.cs
+++ HashCode.Photos/HashCode.Tests/PhotoReaderTest.cs
@@ -23,6 +23,24 @@ namespace HashCode.Tests
             Assert.Equal("cat", photos[0].Tags[0]);
             Assert.Equal("beach", photos[0].Tags[1]);
             Assert.Equal("sun", photos[0].Tags[2]);
-            }
+
+            Assert.Equal(1, photos[1].Id);
+            Assert.Equal(Orientation.Vertical, photos[1].Orientation);
+            Assert.Equal(2, photos[1].Tags.Count);
+            Assert.Equal("selfie", photos[1].Tags[0]);
+            Assert.Equal("smile", photos[1].Tags[1]);
+
+            Assert.Equal(2, photos[2].Id);
+            Assert.Equal(Orientation.Vertical, photos[2].Orientation);
+            Assert.Equal(2, photos[2].Tags.Count);
+            Assert.Equal("garden", photos[2].Tags[0]);
+            Assert.Equal("selfie", photos[2].Tags[1]);
+
+            Assert.Equal(3, photos[3].Id);
+            Assert.Equal(Orientation.Horizontal, photos[3].Orientation);
+            Assert.Equal(2, photos[3].Tags.Count);
+            Assert.Equal("garden", photos[3].Tags[0]);
+            Assert.Equal("cat", photos[3].Tags[1]);
+        }
     }
 }
