cf85a827ff4e0974722abf270b37a7ca32cd76c9
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-02-28 19:37:33 +0000

    TagScorer complete

--- HashCode.Photos/HashCode.Tests/TagScorerTests.cs
+++ HashCode.Photos/HashCode.Tests/TagScorerTests.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
+using HashCode.Photos;
+using Xunit;
+using Xunit.Extensions;
 
 namespace HashCode.Tests
 {
-    class TagScorerTests
+    public class TagScorerTests
     {
+        [Theory]
+        [MemberData(nameof(TagLists))]
+        public void WhenScoring_ShouldReturnCorrectScore(HashSet<string> tag1, HashSet<string> tag2, int expected)
+        {
+            TagScorer.Score(tag1, tag2)
+                .Should().Be(expected);
+
+        }
+
+        public static IEnumerable<object[]> TagLists =>
+        new [] {
+            new object[] { new HashSet<string>(), new HashSet<string> {"a","b"}, 0 },
+            new object[] { new HashSet<string> {"a","b"}, new HashSet<string> (), 0 },
+            new object[] { new HashSet<string> {"a","b"}, new HashSet<string> {"a","b"}, 0 },
+            new object[] { new HashSet<string> {"a","c"}, new HashSet<string> {"a","b"}, 1 },
+            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b"}, 0 },
+            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e"}, 1 },
+            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e", "f"}, 2 },
+        };
     }
 }
