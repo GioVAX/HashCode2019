32c330b0e9e93727dc84dafd6ca380634f3fc25c
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-02-28 19:42:16 +0000

    Clean up

--- HashCode.Photos/HashCode.Tests/TagScorerTests.cs
+++ HashCode.Photos/HashCode.Tests/TagScorerTests.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using HashCode.Photos;
 using Xunit;
-using Xunit.Extensions;
 
 namespace HashCode.Tests
 {
@@ -12,22 +9,21 @@ namespace HashCode.Tests
     {
         [Theory]
         [MemberData(nameof(TagLists))]
-        public void WhenScoring_ShouldReturnCorrectScore(HashSet<string> tag1, HashSet<string> tag2, int expected)
+        public void WhenScoringSetOfTags_ShouldReturnCorrectScore(HashSet<string> tag1, HashSet<string> tag2, int expected)
         {
             TagScorer.Score(tag1, tag2)
                 .Should().Be(expected);
-
         }
 
         public static IEnumerable<object[]> TagLists =>
-        new [] {
-            new object[] { new HashSet<string>(), new HashSet<string> {"a","b"}, 0 },
-            new object[] { new HashSet<string> {"a","b"}, new HashSet<string> (), 0 },
-            new object[] { new HashSet<string> {"a","b"}, new HashSet<string> {"a","b"}, 0 },
-            new object[] { new HashSet<string> {"a","c"}, new HashSet<string> {"a","b"}, 1 },
-            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b"}, 0 },
-            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e"}, 1 },
-            new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e", "f"}, 2 },
-        };
+            new[] {
+                new object[] { new HashSet<string>(), new HashSet<string> {"a","b"}, 0 },
+                new object[] { new HashSet<string> {"a","b"}, new HashSet<string> (), 0 },
+                new object[] { new HashSet<string> {"a","b"}, new HashSet<string> {"a","b"}, 0 },
+                new object[] { new HashSet<string> {"a","c"}, new HashSet<string> {"a","b"}, 1 },
+                new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b"}, 0 },
+                new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e"}, 1 },
+                new object[] { new HashSet<string> {"a","c", "d", "b"}, new HashSet<string> {"a","b", "e", "f"}, 2 },
+            };
     }
 }
