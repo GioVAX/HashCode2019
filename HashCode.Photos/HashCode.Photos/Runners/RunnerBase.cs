c4241b4210991e2809af43a23286d524d6c39e2d
Peter Swallow <p.swallow@elsevier.com>
2019-03-01 10:11:21 +0000

    Created RunnerBase

---
+++ HashCode.Photos/HashCode.Photos/Runners/RunnerBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HashCode.Photos.Runners
+{
+
+    public class RunnerBase
+    {
+        private readonly string _inputFilePath;
+        private readonly string _outputFilePath;
+
+        public IEnumerable<Photo> Photos;
+
+        public RunnerBase(string inputFilePath, string outputFilePath)
+        {
+            _inputFilePath = inputFilePath;
+            _outputFilePath = outputFilePath;
+
+            this.Photos = new PhotoReader().Read(_inputFilePath);
+        }
+
+        public void Save(IEnumerable<Slide> slides)
+        {
+            var slideWriter = new SlideWriter();
+            var outputLines = slideWriter.Format(slides);
+
+            File.WriteAllLines(this._outputFilePath, outputLines);
+        }
+    }
+}
---
+++ HashCode.Photos/HashCode.Photos/Runners/SimpleFlatRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HashCode.Photos.Runners
+{
+    public class SimpleFlatRunner : RunnerBase
+    {
+        public SimpleFlatRunner(string inputFilePath, string outputFilePath)
+            : base(inputFilePath, outputFilePath)
+        {
+        }
+
+        public void Run()
+        {
+            var slides = new List<Slide>();
+            foreach (var photo in this.Photos)
+            {
+                slides.Add(new Slide
+                {
+                    Photos = new List<Photo> { photo }
+                });
+            }
+
+            var solutionSlides = new SlideShow().Compose(slides);
+
+            this.Save(solutionSlides);
+        }
+    }
+}
